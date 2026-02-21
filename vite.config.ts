11e8eba83dd7e6a2a76d4e0479015d45e1c6874c
theelh <meroelhosni123@gmail.com>
2026-02-21 14:11:52 +0000

    updt

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/my-portfolio/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
